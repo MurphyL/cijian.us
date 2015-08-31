b7a152397d671f8cb661ac8a1fe84213a55d0ec2
MurphyL <im@murphyl.com>
2015-08-31 15:15:03 +0800

    隐藏topNav

--- js/loaded.js
+++ js/loaded.js
@@ -1,1 +1,13 @@
 /** **/
+$(function(){
+  $(document).scroll(function(e){
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    if(scrollTop > 200){
+      $('.nav-fixed').css({
+        '-webkit-transform': 'translate3d(0, -130px, 0)',
+        '-moz-transform': 'translate3d(0, -130px, 0)',
+        'transform': 'translate3d(0, -130px, 0)'
+      });
+    }
+  })
+});
