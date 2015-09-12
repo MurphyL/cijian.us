fa0bae91fb09149c41654cc2ec5688a46c4dd5be
MurphyL <im@murphyl.com>
2015-09-13 03:44:08 +0800

    文章迁移、迁移文章的工具代码等

---
+++ deal_data/create_jekyll_data.js
@@ -0,0 +1,89 @@
+(function(fs, striptags, path, oldPostDataFile, posts){
+
+	var joins = [
+		'---\nlayout: post \ntitle: "',
+		'"\ndate: ',
+		'\ncategories: ',
+		'\n---\n\n'
+	];
+	var done = [
+		'1295',
+		'1289',
+		'1284',
+		'1283',
+		'1280',
+		'1276',
+		'1272',
+		'1270',
+		'1268',
+		'1266',
+		'1261',
+		'1258',
+		'1253',
+		'1248',
+		'1022',
+		'1235',
+		'439',
+		'443',
+		'402',
+		'344',
+		'254',
+		'192',
+		'113',
+		'92',
+		'1262',
+		'1260',
+		'1238',
+		'1096'
+
+
+	];
+	fs.readFile(oldPostDataFile, "utf-8", function(err, data){
+		posts = eval(data);
+		var articles = ['Thread Key, 标题, URL'];
+		for (var i = posts.length - 1; i >= 0; i--) {
+			
+			var post = posts[i];
+			var id = post['wp:post_id'][0];
+			console.log(i, id, post['title'][0], done.indexOf(id));
+			if(done.indexOf(id) > -1){
+				continue;
+			}
+			var cates = post['category'];
+			var categoryString = '';
+			for (var j = cates.length - 1; j >= 0; j--) {
+				var cat = cates[j];
+				if(/の/.test(cat['_'])){
+					continue;
+				}
+				categoryString += cat['_'] + ' ';
+			};
+			var contentHTML = post['content:encoded'][0];
+			var linesContent = striptags(contentHTML)/*.split(/\r\n|\n/);
+			var lines = [];
+			for (var k = 0; k < linesContent.length; k++) {
+				var line = linesContent[k];
+				if(/&nbsp;/.test(line)){
+					continue;
+				}
+				if(line.trim().length > 0){
+					lines.push(line.trim());
+				}
+			};*/
+			
+
+			var file_content = joins[0] + post['title'][0] + 
+							   joins[1] + post['wp:post_date'][0] + 
+							   joins[2] + categoryString.trim() + 
+							   joins[3] + /*lines.join('\r\n\r\n').trim()*/ linesContent.trim();
+			var fn_prefix = post['wp:post_date'][0].split(' ')[0] + '-';
+			var fn_seffix = post['wp:post_name'][0].replace(/_/g, '-');
+			fs.writeFile(path + fn_prefix + fn_seffix + ".md", file_content.trim(), "UTF-8");
+			articles.push(post['wp:post_id'][0] + ', ' + post['title'][0].replace(/,/g, ' ') + ', '
+				+ fn_seffix);
+		};
+		fs.writeFile(path + "post_data_new.csv", articles.join('\n'), "UTF-8");
+		console.log('Done');
+	})
+
+})(require('fs'), require('striptags'), "./posts/", "./test/post_data.json");
