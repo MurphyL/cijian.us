e899d98ce62aaed3833a029d36a64add2d61dc7d
MurphyL <im@murphyl.com>
2015-09-19 12:30:00 +0800

    多说

--- js/loaded.js
+++ js/loaded.js
@@ -17,3 +17,15 @@ $(function(){
     }
   })
 });
+// duoshuo
+(function(w, embed) {
+	if(!document.querySelector('.ds-thread')){
+		return;
+	}
+	w.duoshuoQuery = {short_name:"cijian"};
+	var ds = document.createElement('script');
+	ds.type = 'text/javascript';ds.async = true;
+	ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + embed;
+	ds.charset = 'UTF-8';
+	(document.getElementsByTagName('head')[0]  || document.getElementsByTagName('body')[0]).appendChild(ds);
+})(window, '//static.duoshuo.com/embed.js');
